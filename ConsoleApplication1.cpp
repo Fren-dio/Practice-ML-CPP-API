59a4123dd47bab473101b339c8cca43d00aa4d45
Fren-dio <100287696+Fren-dio@users.noreply.github.com>
2024-10-16 19:09:43 +0700

    add read input .npy file
    add read data from .npy file, using libnpy for it

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,11 +1,11 @@
-﻿
+﻿// read_nns_model.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
 
-
-#include <iostream>
-#include <string.h>
-#include <vector>
 #include <torch/script.h>
 
+#include <iostream>
+
+#include "npy.hpp"
 
 
 
@@ -16,52 +16,57 @@ bool is_file_exist(std::string fileName)
 }
 
 
-
-
 int main(int argc, char* argv[])
 {
 	try {
 
-		if (argc != 2) {
-			std::cerr << "usage: predict <path-to-exported-script-module> > \n";
-			return -1;
-		}
+		std::string model_path = "model.pt";  // here it's a argv[1]
+		//1. load ckpt NNs model
+		//read model from ckpt file 
+		torch::jit::script::Module model;
+		model = torch::jit::load(model_path);
+		torch::DeviceType cpuDeviceType = torch::kCPU;
+		torch::Device cpuDevice(cpuDeviceType);
+		model.to(cpuDevice);
+		std::cout << "torch::jit::script::Module element was created\n";
 
-		std::string model_path = argv[1];
 
-		//for CPP part of code need .pt file's format
-		if (is_file_exist(model_path))
-			std::cout << "file wasn't found\n";
-		else {
-			std::cout << "file was successfull found\n";
-			//1. load ckpt NNs model
-			//read model from ckpt file 
-		
-			torch::jit::script::Module model;
-			model = torch::jit::load(model_path);
-			torch::DeviceType cpuDeviceType = torch::kCPU;
-			torch::Device cpuDevice(cpuDeviceType);
-			model.to(cpuDevice);
-			std::cout << "torch::jit::script::Module element was created\n";
-		
-			//2. convert to torch tensor
-			torch::DeviceType cpu_device_type = torch::kCPU;
-			torch::Device cpu_device(cpu_device_type);
-			model.to(cpu_device);
-			std::cout << "model was convert to torch tensor\n";
-		
 
-			// 3. create input data for testing loaded model
-			// Create a vector of inputs.
-			std::vector<torch::jit::IValue> inputs;
-			// now it is just random values, but in future must be input data from other file
-			inputs.push_back(torch::ones({ 1, 4, 45, 223 })); 
+		//2. convert to torch tensor
+		torch::DeviceType cpu_device_type = torch::kCPU;
+		torch::Device cpu_device(cpu_device_type);
+		model.to(cpu_device);
+		std::cout << "model was convert to torch tensor\n";
 
-			// Execute the model and turn its output into a tensor.
-			// start pre-learning model, which was loaded before
-			at::Tensor output = module.forward(inputs).toTensor();
-			std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
-		}		
+
+
+		// 3. create input data for testing loaded model
+		// Create a vector of inputs.
+		std::vector<torch::jit::IValue> inputs;
+		// now it is just random values, but in future must be input data from other file
+		int SIZE_ARRAY = 1024;
+		double* dataArray = (double*)calloc(SIZE_ARRAY, sizeof(double));
+
+		//  read input data from 'data.npy' file and initialize array, which will use for create tensor
+		// created tensor will used to start NNs on this data
+		// using libnpy
+		const std::string path{ "data.npy" };
+		std::vector<double> data = (npy::read_npy<double>(path)).data;
+		//convert vector<> to double*
+		dataArray = &data[0];
+
+		at::Tensor inputDataTensor = torch::tensor(dataArray, { torch::kFloat64 });
+
+		inputs.push_back(inputDataTensor);
+		std::cout << "input data was loaded\n";
+
+
+
+
+		// 4. Execute the model and turn its output into a tensor.
+		// start pre-learning model, which was loaded before
+		at::Tensor output = model.forward(inputs).toTensor();
+		std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
 
 	}
 	catch (const c10::Error& e) {
@@ -72,41 +77,4 @@ int main(int argc, char* argv[])
 		std::cout << err << std::endl;
 	}
 
-	return 0;
 }
-
-
-
-
-
-
-//#include <torch/torch.h>
-//#include "torch.h"
-//#include <iostream>
-
-/*template <typename T>
-void pretty_print(const std::string& info, T&& data) {
-    std::cout << info << std::endl;
-    std::cout << data << std::endl << std::endl;
-}
-*/
-
-//int main() {
-    //std::cout << "hello";
-    // Create an eye tensor
-    //torch::Tensor tensor = torch::eye(3);
-    //pretty_print("Eye tensor: ", tensor);
-
-    // Tensor view is like reshape in numpy, which changes the dimension representation of the tensor
-    // without touching its underlying memory structure.
-    //tensor = torch::range(1, 9, 1);
-    //pretty_print("Tensor range 1x9: ", tensor);
-    //pretty_print("Tensor view 3x3: ", tensor.view({ 3, 3 }));
-    //pretty_print("Tensor view 3x3 with D0 and D1 transposed: ", tensor.view({ 3, 3 }).transpose(0, 1));
-    //tensor = torch::range(1, 27, 1);
-    //pretty_print("Tensor range 1x27: ", tensor);
-    //pretty_print("Tensor view 3x3x3: ", tensor.view({ 3, 3, 3 }));
-    //pretty_print("Tensor view 3x3x3 with D0 and D1 transposed: ",
-    //    tensor.view({ 3, 3, 3 }).transpose(0, 1));
-    //pretty_print("Tensor view 3x1x9: ", tensor.view({ 3, 1, -1 }));
-//}
